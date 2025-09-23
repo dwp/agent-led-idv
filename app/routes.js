e6a37710dac38dffa369b6e781cacaf8d4c2dc1a
Ian Brookes <ian.brookes@engineering.digital.dwp.gov.uk>
2025-09-23 11:11:48 +0100

    updated changes
    GitOrigin-RevId: ff525e66d4a11085b04cb6eebbb98f6e891906ca

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,32 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/establish-answer', function(request, response) {
+
+	var matched = request.session.data['matched']
+	if (matched == "yes"){
+		response.redirect("/path1/fraud-check")
+	} else {
+		response.redirect("path1/identity-not-verified")
+	}
+})
+
+router.post('/fraud-answer', function(request, response) {
+
+	var fraud = request.session.data['fraud']
+	if (fraud.includes("nifu")){
+		response.redirect("/path1/nifu-stop")
+	} else {
+		response.redirect("/path1/verify-identity")
+	}
+})
+
+router.post('/verify-answer', function(request, response) {
+
+	var verify = request.session.data['verify']
+	if (verify == "yes"){
+		response.redirect("/path1/identity-verified")
+	} else {
+		response.redirect("/path1/identity-not-verified")
+	}
+})
