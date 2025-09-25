b313dc056e0c7730666baf7c5238c3e57a6c430c
Ian Brookes <ian.brookes@engineering.digital.dwp.gov.uk>
2025-09-25 16:50:33 +0100

    error page for fraud checks
    GitOrigin-RevId: e547a3d290eadaba40b3df7c18c6e89b610a4c22

--- app/routes.js
+++ app/routes.js
@@ -17,15 +17,26 @@ router.post('/establish-answer', function(request, response) {
 	}
 })
 
-router.post('/fraud-answer', function(request, response) {
+router.post('/fraud-answer', function(request, response) {
+    var fraud = request.session.data['fraud'];
 
-	var fraud = request.session.data['fraud']
-	if (fraud.includes("nifu")){
-		response.redirect("/path1/nifu-stop")
-	} else {
-		response.redirect("/path1/verify-identity")
-	}
-})
+    // If no selection is made
+    if (!fraud || fraud.length === 0) {
+        response.redirect('/path1/fraud-check-error');
+    } else if (fraud.includes("nifu")) {
+        response.redirect("/path1/nifu-stop");
+    } else if (fraud.includes("dateofdeath")) {
+        response.redirect("/path1/dateofdeath");
+    } else if (fraud.includes("idatrisk")) {
+        response.redirect("/path1/verify-identity");
+    } else if (fraud.includes("cfems")) {
+        response.redirect("/path1/cfems");
+    } else if (fraud.includes("none")) {
+        response.redirect("/path1/verify-identity-no-markers");
+    } else {
+        response.redirect('/path1/fraud-check-error');
+    }
+});
 
 router.post('/verify-answer', function(request, response) {
 
